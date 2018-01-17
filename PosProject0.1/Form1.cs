a504863f363c4df93e6507dab84478d8b32d4bc0
kdw <abc@abc.com>
2018-01-17 17:50:51 +0900

    잘못올린 파일 수정

---
+++ PosProject0.1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PosProject0._1
+{
+    public partial class Form1 : Form
+    {
+        frmCheckIn fck = new frmCheckIn();
+        frmSalesManager fsm;
+        SalesStatus statusSale;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fck.MdiParent = this;
+            fck.Dock = DockStyle.Fill;          
+            fck.Show();           
+        }
+
+        private void btnSalesMan_Click(object sender, EventArgs e)
+        {
+            statusSale = new SalesStatus();
+            statusSale.MdiParent = this;
+            statusSale.Dock = DockStyle.Fill;
+
+            fsm = new frmSalesManager(statusSale);
+            fsm.MdiParent = this;
+            fsm.Dock = DockStyle.Fill;
+            for (int i = 0; i < MdiChildren.Length; i++)
+            {
+                MdiChildren[i].Hide();
+            }
+            fsm.Show();
+        }
+
+        private void btnCheckIn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < MdiChildren.Length; i++)
+            {
+                MdiChildren[i].Hide();
+            }
+            fck.Show();
+        }
+    }
+}
